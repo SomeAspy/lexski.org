31eedf9f8e41f754e5bce23107954eb57b2c8cdf
Aiden <amb@aspy.dev>
2022-04-24 12:17:41 -0400

    breaking ground

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import { render } from 'solid-js/web'
+import { lazy } from 'solid-js'
+import { Routes, Route, Router } from 'solid-app-router'
+
+const Home = lazy(() => import('./pages/home.jsx'));
+
+render(
+    () => (
+        <Router>
+            <Routes>
+                <Route path="/" component={Home} />
+            </Routes>
+        </Router>
+    ),
+    document.getElementById('root')
+)
